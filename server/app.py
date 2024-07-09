192e77cf97c278550d1141a9ad1a7735e8be14ff
Bianca Kioko <kiokobianca01@gmail.com>
2024-07-09 23:01:29 +0300

    Updated the full crud functionality to the routes

--- server/app.py
+++ server/app.py
@@ -29,13 +29,54 @@ def index():
     return render_template('index.html')
 
 #Add the routes and views here
+#Courses
 class Courses(Resource):
     def get(self):
         courses_dict_list = [course.to_dict() for course in Course.query.all()]
         response = make_response(courses_dict_list,200)
         return response
     
+    def post(self):
+        new_course = Course(
+            name = request.form['name'],
+            description = request.form['description'],
+            teacher_id = request.form['teacher_id'],
+            course_code = request.form['course_code'],
+        )
+        db.session.add(new_course)
+        db.session.commit()
+
+        res_dict = new_course.to_dict()
+        response = make_response(res_dict,201)
+        return response
+    
 api.add_resource(Courses, '/courses')
+
+class CourseById(Resource):
+    def get(self, id):
+        course = Course.query.filter_by(course_id=id).first().to_dict()
+        return make_response(jsonify(course), 200)
+    
+    def patch(self, id):
+        course = Course.query.filter(Course.course_id == id).first()
+        for attr in request.form:
+            setattr(course,attr,request.form[attr])
+        db.session.add(course)
+        db.session.commit()
+
+        res_dict = course.to_dict()
+        response = make_response(res_dict, 200)
+        return response
+    
+    def delete(self, id):
+        course = Course.query.filter(Course.course_id == id).first()
+        db.session.delete(course)
+        db.session.commit()
+        res_dict = {"message":"course deleted successfully"}
+        response = make_response(res_dict,200)
+        return response
+    
+api.add_resource(CourseById, '/courses/<int:id>')
 
 
 if __name__ == '__main__':
